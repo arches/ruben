cecfe2571588137c429fec686c85e854255a18c9
Chris Doyle <blaimue@gmail.com>
2010-12-05 14:17:21 -0800

    GA

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rcards::Application.routes.draw do
 
-  resources :questions do
-    collection do
-      get :check
-    end
-  end
+#  resources :questions do
+#    collection do
+#      get :check
+#    end
+#  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
